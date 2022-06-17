d10bec4a5f94d001be3f12f78882e96d09348052
Elmira Ilyasova <capricorn_26@mail.ru>
2022-06-17 18:55:59 +0700

    Add card with logic

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
+import { productsMock } from "../../mock";
+
+import Card from "../card/card";
+
 const Products = () => {
   return (
     <div className="products">
       <h1 className="visually-hidden">Products</h1>
       <b className="products__title">Ты сегодня покормил кота?</b>
       <div className="products__container">
-        <div className="test"></div>
-        <div className="test"></div>
-        <div className="test"></div>
+        {productsMock.map((product, i) => <Card key={`card-${i}`} product={product} />)}
       </div>
     </div>
   );
