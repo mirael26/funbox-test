615c7290dfbeebaa5d49a15a347c216e8e5676b5
Elmira Ilyasova <capricorn_26@mail.ru>
2022-06-17 18:55:34 +0700

    Add mocks

---
+++ src/mock.js
@@ -0,0 +1,60 @@
+export const productsMock = [
+  {
+    title: 'Нямушка',
+    flavor: 'с фуа-гра',
+    subtitle: 'Сказочное заморское яство',
+    weight: '0,5',
+    description: 'Печень утки разварная с артишоками.',
+    details: [
+      {
+        number: '10',
+        text: ' порций'
+      },      
+      {
+        number: '',
+        text: 'мышь в подарок'
+      }
+    ],
+    isAvailable: true,
+  },
+  {
+    title: 'Нямушка',
+    flavor: 'с рыбой',
+    subtitle: 'Сказочное заморское яство',
+    weight: '2',
+    description: 'Головы щучьи с чесноком да свежайшая сёмгушка.',
+    details: [
+      {
+        number: '40',
+        text: ' порций'
+      },
+      {
+        number: '2',
+        text: ' мыши в подарок'
+      }
+    ],
+    isAvailable: true,
+  },
+  {
+    title: 'Нямушка',
+    flavor: 'с курой',
+    subtitle: 'Сказочное заморское яство',
+    weight: '5',
+    description: 'Филе из цыплят с трюфелями в бульоне.',
+    details: [
+      {
+        number: '100',
+        text: ' порций'
+      },
+      {
+        number: '5',
+        text: ' мышей в подарок'
+      },
+      {
+        number: '',
+        text: 'заказчик доволен'
+      }
+    ],
+    isAvailable: false,
+  }
+];
